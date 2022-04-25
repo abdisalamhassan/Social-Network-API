e7419d87be4a2af8adf1babce4d80933b9ed558b
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-04-24 21:23:02 -0400

    server.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -57,9 +57,31 @@ const ThoughtController = {
              })
           .catch(err => res.status(400).json(err));
          
+        },
 
-         
-        }
+        removeThought({ params }, res ){
+            Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then(deledThought => {
+                if(!deleteThought){
+                    return res.status(404).json({ message: 'No thought found with this id' });
+                }
+                return User.findOneAndUpdate(
+                    {_id: params.userId },
+                    { $pull: {thoughts: params.thoughtId }},
+                    {new: true}
+                );
+            })
+            .then(dbUserData=> {
+                if(!dbUserData){
+                    res.status(404).json({message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+        
 
 
-    }
+    },
+
+    
